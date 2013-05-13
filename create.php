0471509f4b3af713b10bb472e8e9c7962b983e1d
godka <thegodka@gmail.com>
2013-05-14 02:34:31 +0300

    pretty JSON

--- create.php
+++ create.php
@@ -12,12 +12,15 @@ $refs = [];
 foreach ($files as $item) {
     if ($item != 'images') {
         $pieces = explode('.', $item,-1);
+        
+        /*
         // usual offset at 664-828
         $data=file_get_contents('phar://php_manual_en.tar.gz/php-chunked-xhtml/' . $item, false, NULL, 640, 512); 
         
         if (preg_match('`<h1[^>]*>([^< ]+)</h1>`', $data, $refname)) {
             // TODO? Title
         }
+        */
         
         foreach ($pieces as $piece) $refs[str_replace('-','',$piece)]=0b0;
     }
@@ -25,9 +28,9 @@ foreach ($files as $item) {
 
 foreach ($remove as $out) unset($refs[$out]);
 
-$output = '{ "';
-$output.= implode('":"1",' . "\n", array_keys($refs));
-$output.= '" }'."\n";
+$output = "{\n" . '"';
+$output.= implode('":"1",' . "\n" . '"', array_keys($refs));
+$output.= '":"1"' . "\n}";
 
 file_put_contents('PHPreferences.json', $output);
 
