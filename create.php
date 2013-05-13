2c07184851fa8896dc08c28e724d72d51efb5a9b
godka <thegodka@gmail.com>
2013-05-14 01:53:23 +0300

    toJSON

--- create.php
+++ create.php
@@ -1,5 +1,4 @@
 <?php
-
 $remove = ['about', 'html', 'more', 'notes', 'translations', 'examples', 'installation', 'requirements', 'resources', 
            'setup', 'connect', 'disconnect', 'shutdown', 'timezones', 'africa', 'america', 'antarctica', 'arctic', 
            'asia', 'atlantic', 'australia', 'europe', 'indian', 'pacific', 'others', 'get', 'concept_cache', 
@@ -11,18 +10,26 @@ $remove = ['about', 'html', 'more', 'notes', 'translations', 'examples', 'instal
 $files = scandir('phar://php_manual_en.tar.gz/php-chunked-xhtml');
 $refs = [];
 foreach ($files as $item) {
-    $pieces = explode('.', $item,-1);
-    foreach ($pieces as $piece) $refs[str_replace('-','',$piece)]=0b0;
+    if ($item != 'images') {
+        $pieces = explode('.', $item,-1);
+        // usual offset at 664-828
+        $data=file_get_contents('phar://php_manual_en.tar.gz/php-chunked-xhtml/' . $item, false, NULL, 640, 512); 
+        
+        if (preg_match('`<h1[^>]*>([^< ]+)</h1>`', $data, $refname)) {
+            // TODO? Title
+        }
+        
+        foreach ($pieces as $piece) $refs[str_replace('-','',$piece)]=0b0;
+    }
 }
 
 foreach ($remove as $out) unset($refs[$out]);
 
-$output = "var PHPreferences = { '";
-$output.= implode("' : 1, \n'", array_keys($refs));
-$output.= "' };\n";
+$output = '{ "';
+$output.= implode('":"1",' . "\n", array_keys($refs));
+$output.= '" }'."\n";
 
-file_put_contents('PHPreferences.js', $output);
+file_put_contents('PHPreferences.json', $output);
 
 print "\nMemory: " . number_format(memory_get_usage()/1048576, 2, '.', ' ') . 
       " Mb \t[Peak: " . number_format(memory_get_peak_usage()/1048576, 2, '.', ' ') . " Mb]\n";
-// not ending the script tag like a baws
